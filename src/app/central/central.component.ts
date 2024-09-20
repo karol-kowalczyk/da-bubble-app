1213dbae53982d8b56f40b344ac9598cf1c4b09a
phischa <ph.git@mailbox.org>
2024-09-20 22:55:00 +0200

    first workspace-branch commit

---
+++ src/app/central/central.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-central',
+  standalone: true,
+  imports: [],
+  templateUrl: './central.component.html',
+  styleUrl: './central.component.scss'
+})
+export class CentralComponent {
+
+}
