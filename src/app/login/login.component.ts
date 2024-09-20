08f1e06f3c48893e7e5f7b41f401e79fb398b8a2
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-09-20 22:08:36 +0200

    login section

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { LoginHeaderComponent } from './shared/login-header/login-header.component';
+import { LoginFooterComponent } from './shared/login-footer/login-footer.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginHeaderComponent, LoginFooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
---
+++ src/app/login/shared/login-header/login-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-header.component.html',
+  styleUrl: './login-header.component.scss'
+})
+export class LoginHeaderComponent {
+
+}
