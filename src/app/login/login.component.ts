c4d2361fcf58dee3709902d485693ccb347fd18b
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-09-22 11:02:36 +0200

    login-page-full-responsive

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LoginHeaderComponent } from './shared/login-header/login-header.component';
 import { LoginFooterComponent } from './shared/login-footer/login-footer.component';
+import { Renderer2 } from '@angular/core';
+import { ResponsiveCreateUserSectionComponent } from './shared/responsive-create-user-section/responsive-create-user-section.component';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginHeaderComponent, LoginFooterComponent],
+  imports: [LoginHeaderComponent, LoginFooterComponent, ResponsiveCreateUserSectionComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
 
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.renderer.addClass(document.body, 'login-page');
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(document.body, 'login-page');
+  }
 }
---
+++ src/app/login/shared/responsive-create-user-section/responsive-create-user-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsive-create-user-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './responsive-create-user-section.component.html',
+  styleUrl: './responsive-create-user-section.component.scss'
+})
+export class ResponsiveCreateUserSectionComponent {
+
+}
