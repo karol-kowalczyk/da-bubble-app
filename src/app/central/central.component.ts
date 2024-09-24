9422b8097945da4908da2fe83f28c62256310fc5
phischa <ph.git@mailbox.org>
2024-09-24 16:22:02 +0200

    added channel and thread container

--- src/app/central/central.component.ts
+++ src/app/central/central.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { WorkspaceComponent } from './workspace/workspace.component';
+import { ChannelComponent } from './channel/channel.component';
+import { ThreadComponent } from './thread/thread.component';
 
 @Component({
   selector: 'app-central',
   standalone: true,
-  imports: [ WorkspaceComponent, ],
+  imports: [ WorkspaceComponent, ChannelComponent, ThreadComponent],
   templateUrl: './central.component.html',
   styleUrl: './central.component.scss'
 })
