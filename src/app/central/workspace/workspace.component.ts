fed776c297a6e1e0f6a88c4447fab72c471f33a2
phischa <ph.git@mailbox.org>
2024-09-25 17:10:28 +0200

    added animation

--- src/app/central/workspace/workspace.component.ts
+++ src/app/central/workspace/workspace.component.ts
@@ -5,24 +5,29 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss',
   animations: [
     trigger('slideInOut', [
-        state('in', style({ height: '*', opacity: 1 })),
-        state('out', style({ height: '0px', opacity: 0 })),
-        transition('in <=> out', [
-            animate('300ms ease-in-out')
-        ])
+      state('in', style({ height: '*', opacity: 1 })),
+      state('out', style({ height: '0px', opacity: 0 })),
+      transition('in <=> out', [
+        animate('300ms ease-in-out')
+      ])
     ])
-]
+  ]
 })
 export class WorkspaceComponent {
   isMenuVisible = false;
-    contents = ['Inhalt 1', 'Inhalt 2', 'Inhalt 3'];
+  isDirectMenuVisible = false;
+  contents = ['Inhalt 1', 'Inhalt 2', 'Inhalt 3'];
 
-    toggleMenu() {
-        this.isMenuVisible = !this.isMenuVisible;
-    }
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  toggleDirectMenu() {
+    this.isDirectMenuVisible = !this.isDirectMenuVisible;
+  }
 }
