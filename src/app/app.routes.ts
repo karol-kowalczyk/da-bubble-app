e579f69e578b11c7f7eda57c6e7cd881cf75f540
phischa <ph.git@mailbox.org>
2024-09-25 05:48:10 +0200

    deleted route from branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { CentralComponent } from './central/central.component';
 
 export const routes: Routes = [
-    { path: '', component: CentralComponent },
 ];
