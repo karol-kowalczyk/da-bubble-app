023d9a78bdd556fc7fc4ad6cd7f6c770993c22d0
phischa <ph.git@mailbox.org>
2024-09-25 07:26:08 +0200

    in out transition

--- src/app/central/workspace/workspace.component.ts
+++ src/app/central/workspace/workspace.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './workspace.component.html',
-  styleUrl: './workspace.component.scss'
+  styleUrl: './workspace.component.scss',
+  animations: [
+    trigger('slideInOut', [
+        state('in', style({ height: '*', opacity: 1 })),
+        state('out', style({ height: '0px', opacity: 0 })),
+        transition('in <=> out', [
+            animate('300ms ease-in-out')
+        ])
+    ])
+]
 })
 export class WorkspaceComponent {
+  isMenuVisible = false;
+    contents = ['Inhalt 1', 'Inhalt 2', 'Inhalt 3'];
 
+    toggleMenu() {
+        this.isMenuVisible = !this.isMenuVisible;
+    }
 }
