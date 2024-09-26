4bec2711532fd0bdb5a8a244cc7a957af2e6f785
phischa <ph.git@mailbox.org>
2024-09-26 15:27:15 +0200

    added primary/secondary btn to style.scss

--- src/app/central/workspace/add-channel-dialog/add-channel-dialog.component.ts
+++ src/app/central/workspace/add-channel-dialog/add-channel-dialog.component.ts
@@ -2,27 +2,13 @@ import { Component, inject, NgModule, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-import { MatNativeDateModule, DateAdapter, NativeDateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatCardModule } from '@angular/material/card';
-
 @Component({
   selector: 'app-add-channel-dialog',
   standalone: true,
-  imports: [MatCardModule, CommonModule,
-    FormsModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatNativeDateModule],
+  imports: [ CommonModule,FormsModule,],
   templateUrl: './add-channel-dialog.component.html',
   styleUrl: './add-channel-dialog.component.scss'
 })
 export class AddChannelDialogComponent {
-
+  
 }
