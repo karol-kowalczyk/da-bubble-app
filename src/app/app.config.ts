c91cbfa3d9720bc0f9ca29b6514c75cce59d15b2
Kai Janicek <100490095+kaix6@users.noreply.github.com>
2024-09-27 13:33:51 +0200

    repair repository

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,12 @@ import { provideRouter } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(BrowserAnimationsModule)
+    importProvidersFrom(BrowserAnimationsModule), provideAnimationsAsync()
   ]
 };
