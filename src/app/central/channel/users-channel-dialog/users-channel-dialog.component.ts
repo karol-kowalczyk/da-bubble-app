5ee483250c55e3956946a4ed6605840ec1ae6ac8
phischa <ph.git@mailbox.org>
2024-10-03 17:39:23 +0200

    new sub components

---
+++ src/app/central/channel/users-channel-dialog/users-channel-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-channel-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-channel-dialog.component.html',
+  styleUrl: './users-channel-dialog.component.scss'
+})
+export class UsersChannelDialogComponent {
+
+}
