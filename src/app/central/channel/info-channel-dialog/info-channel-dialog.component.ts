bbf5b3843c3267ae31b5ad6c0463a439ab0c0872
phischa <ph.git@mailbox.org>
2024-10-06 18:08:34 +0200

    new sub components css

--- src/app/central/channel/info-channel-dialog/info-channel-dialog.component.ts
+++ src/app/central/channel/info-channel-dialog/info-channel-dialog.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgModule, } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-info-channel-dialog',
   standalone: true,
-  imports: [],
+  imports: [ MatDialogModule, /* CommonModule, FormsModule, */ ],
   templateUrl: './info-channel-dialog.component.html',
   styleUrl: './info-channel-dialog.component.scss'
 })
 export class InfoChannelDialogComponent {
-
+  constructor(public dialogRef: MatDialogRef<InfoChannelDialogComponent>) {
+    
+  }
+  
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
