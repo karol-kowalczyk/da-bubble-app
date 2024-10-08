64f5843403e2b01dcef7b21055d13bf4ba1ef4c1
Kai Janicek <100490095+kaix6@users.noreply.github.com>
2024-10-08 17:47:00 +0200

    closeThread

--- src/app/central/thread/thread.component.ts
+++ src/app/central/thread/thread.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-thread',
   standalone: true,
-  imports: [],
+  imports: [AppComponent],
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
 export class ThreadComponent {
+
+constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+closeThreadDialog() {
+  const threadContainer = this.el.nativeElement.querySelector('#threadContainer');
+  this.renderer.addClass(threadContainer, "d-none")
+}
 
 }
