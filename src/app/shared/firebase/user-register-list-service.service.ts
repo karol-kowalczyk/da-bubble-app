8af3010e21276680ea916854f90ee24105c33a9a
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-11-01 16:54:19 +0100

    created a user service list to modify the collection in our firebase server'

--- src/app/shared/firebase/user-register-list-service.service.ts
+++ src/app/shared/firebase/user-register-list-service.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, collectionData, doc, onSnapshot } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegisterListServiceService {
+export class UserRegisterListService {
 
-  constructor() { }
+  firestore:Firestore = inject(Firestore);
+  unsubList;
+
+  constructor() { 
+
+    this.unsubList = onSnapshot(this.getUserRef(), (list) => {
+      list.forEach(element => {
+        console.log(element.id);
+        console.log(element.data())
+      });
+    })
+  }
+
+  ngonDestroy() {
+    this.unsubList();
+  }
+
+  getUserRef() {
+    return collection(this.firestore, 'users');
+  }
+
+  getSingleDoc(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId)
+  }
 }
